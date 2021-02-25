96a108ded31ad9bc284dfc88730b937a254b1f35
kernzerfall <kernzerfall@gmx.de>
2021-02-25 20:40:35 +0200

    Remove unused function exports

--- include/resources/resources.hpp
+++ include/resources/resources.hpp
@@ -11,12 +11,6 @@
 #include <fstream>
 #endif
 
-#ifdef __WIN32
-static f32 CalculateCPULoad(u64 idleTicks, u64 totalTicks);
-static u64 FileTimeToInt64(const FILETIME &ft);
-f32 GetCPULoad();
-#endif
-
 namespace Resources {
 	byte getCPUtil();
 	byte getRAMUtil();
